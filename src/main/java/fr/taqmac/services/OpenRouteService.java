a800d242f5d83e2250f56c588084b95d20722d8e
Mathieu OLIVES-DELMAS <mathieuolives-delmas@macbook-pro-de-mathieu.home>
2020-03-27 15:22:27 +0100

    modify api

---
+++ src/main/java/fr/taqmac/services/OpenRouteService.java
@@ -0,0 +1,49 @@
+package fr.taqmac.services;
+
+import fr.taqmac.utils.ResponseHttpUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+
+@RestController
+public class OpenRouteService {
+
+    public static final String getBaseURL = "https://api.openrouteservice.org";
+    public static final String getKey = "5b3ce3597851110001cf6248c3e0f4ea0c98469b87828a7ec113cc29";
+
+    @GetMapping(value = "/geocode/autocomplete/{text}")
+    private ResponseEntity<String> autocomplete(@PathVariable String text) throws IOException {
+
+        ResponseHttpUtils response = HTTPService.call(getBaseURL + "/geocode/autocomplete?text=" + text +
+                "&api_key=" + getKey, HTTPService.GET);
+        String listAutoComplete = response.getResultContent();
+        System.out.println(listAutoComplete);
+        JSONObject listAutoCompleteJson = new JSONObject(listAutoComplete);
+        JSONArray listAutoCompleteArray = listAutoCompleteJson.getJSONArray("features");
+        JSONArray resultListAutoComplete = new JSONArray();
+        for (int i = 0 ; i < listAutoCompleteArray.length(); i++) {
+            JSONObject element = listAutoCompleteArray.getJSONObject(i);
+            JSONObject resultJo = new JSONObject();
+            JSONArray coordonnees = element.getJSONObject("geometry").getJSONArray("coordinates");
+            resultJo.put("x",coordonnees.getFloat(0));
+            resultJo.put("y",coordonnees.getFloat(1));
+            resultJo.put("label",element.getJSONObject("properties").getString("label"));
+            resultListAutoComplete.put(resultJo);
+        }
+
+
+        if (response.getResultCode() == HttpStatus.OK.value())
+            return HTTPService.createResponse(resultListAutoComplete.toString(), HttpStatus.OK);
+        else
+            return HTTPService.createResponse("", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
+}
--- src/main/java/fr/taqmac/services/TisseoApiParserService.java
+++ src/main/java/fr/taqmac/services/TisseoApiParserService.java
@@ -2,6 +2,9 @@ package fr.taqmac.services;
 
 import fr.taqmac.parser.TisseoParser;
 import fr.taqmac.utils.ResponseHttpUtils;
+import org.apache.tomcat.util.buf.StringUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 @RestController
@@ -29,15 +33,45 @@ public class TisseoApiParserService {
 			return HTTPService.createResponse("", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
-	@GetMapping(value = "/tisseo/listPoints/{localisationStart}/{localisationEnd}")
+	// Format : 1.43697,43.5849 par point
+	// FORMAT TransportModeList: métro,bus,tramway séparé par des virgules dans l'url.
+	@GetMapping(value = "/tisseo/listPoints/{localisationStart}/{localisationEnd}/{transportModeList}")
 	private ResponseEntity<String> listPoints(@PathVariable String localisationStart,
-											  @PathVariable String localisationEnd)
+											  @PathVariable String localisationEnd,
+											  @PathVariable String[] transportModeList)
 			throws IOException {
-		ResponseHttpUtils response = HTTPService.call(getBaseURL + "/journeys.json?departurePlace=" + localisationStart
-				+ "&arrivalPlace=" + localisationEnd + "&displayWording=1&key=" + getKey, HTTPService.GET);
+
+		String rollingStocksURL = getBaseURL + "/rolling_stocks.json";
+		ResponseHttpUtils responseRollingStocksURL = HTTPService.call(rollingStocksURL, HTTPService.GET);
+		JSONObject rollingStocksObject = new JSONObject(responseRollingStocksURL.getResultContent());
+		JSONArray rollingStocks = rollingStocksObject.getJSONArray("rollingStocks");
+
+		System.out.println(rollingStocks);
+
+		String url = getBaseURL + "/journeys.json?departurePlaceXY=" + localisationStart
+				+ "&arrivalPlaceXY=" + localisationEnd + "&displayWording=1&key=" + getKey + "&rollingStockList=";
+
+		ArrayList<String> listId = new ArrayList<>();
+		for(String t : transportModeList) {
+			for (int i = 0 ; i < rollingStocks.length(); i++) {
+				JSONObject obj = rollingStocks.getJSONObject(i);
+				String name = obj.getString("name");
+				String id = obj.getString("id");
+				System.out.println(id);
+				if (name.equals(t)){
+					listId.add(id);
+					//url += id + ",";
+				}
+			}
+		}
+		url += StringUtils.join(listId, ',');
+		System.out.println(url);
+
+		ResponseHttpUtils response = HTTPService.call(url, HTTPService.GET);
 		String journeys = response.getResultContent();
 
 		System.out.printf(TisseoParser.getJourneysText(journeys).toString());
+
 		if (response.getResultCode() == HttpStatus.OK.value())
 			return HTTPService.createResponse(journeys, HttpStatus.OK);
 		else
