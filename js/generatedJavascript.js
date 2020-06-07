4abf58feea90888dcea3ce7caa1308b9ae0881e4
Quentin <quentin.ceschin@gmail.com>
2020-06-07 17:43:59 +0200

    NTM LE LINTER

--- js/generatedJavascript.js
+++ js/generatedJavascript.js
@@ -1,13 +1,17 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
 function setCheckboxList() {
     document.getElementById("transportationListContainer").innerHTML = "<label for='bus'>Bus</label><input type='checkbox' name='bus' value='bus' id='bus'></<label><label for='metro'>Metro</label><input type='checkbox' name='metro' value='metro' id='metro'>";
 }
 setCheckboxList();
-
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
 function createSelectDisplayMode() {
     document.getElementById("route").style.display = "none"
 }
 createSelectDisplayMode();
-
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-unused-vars
 function createTimeSlots() {
     document.getElementById("timeSlotsContainer").innerHTML = '<label for=horaire"">Horaire de départ</label> <input type="time" id="timeSlotImput" name="horaire" min="09:00" max="18:00" required>';
 }
